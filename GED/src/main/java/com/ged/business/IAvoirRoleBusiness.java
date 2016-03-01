c43cd6caa7693e9198d0e8b978bd91485d3338c6
Samir <Samir@MacBook>
2016-03-01 01:09:21 +0100

    suite1 implémentation business

--- GED/src/main/java/com/ged/business/IAvoirRoleBusiness.java
+++ GED/src/main/java/com/ged/business/IAvoirRoleBusiness.java
@@ -1,5 +1,18 @@
 package com.ged.business;
 
+import java.util.Collection;
+
+import com.ged.domain.AvoirRole;
+import com.ged.domain.Bibliotheque;
+import com.ged.domain.Role;
+import com.ged.domain.Utilisateur;
+
 public interface IAvoirRoleBusiness {
+
+	AvoirRole attribuerRoleUtilisateur(Utilisateur utilisateur, Role role, Bibliotheque bibliotheque);
+
+	void retirerRoleUtilisateur(Utilisateur utilisateur, Role role, Bibliotheque bibliotheque);
+
+	Collection<Role> getRolesUtilisateur(Utilisateur utilisateur, Bibliotheque bibliotheque);
 
 }
---
+++ GED/src/main/java/com/ged/business/impl/AvoirRoleBusinessImpl.java
@@ -0,0 +1,44 @@
+package com.ged.business.impl;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ged.business.IAvoirRoleBusiness;
+import com.ged.dao.AvoirRoleRepository;
+import com.ged.domain.AvoirRole;
+import com.ged.domain.AvoirRolePK;
+import com.ged.domain.Bibliotheque;
+import com.ged.domain.Role;
+import com.ged.domain.Utilisateur;
+
+public class AvoirRoleBusinessImpl implements IAvoirRoleBusiness {
+
+	@Autowired
+	private AvoirRoleRepository avoirRoleRepository;
+
+	@Override
+	public AvoirRole attribuerRoleUtilisateur(Utilisateur utilisateur, Role role, Bibliotheque bibliotheque) {
+		AvoirRolePK id = new AvoirRolePK();
+		id.setUtilisateur(utilisateur);
+		id.setRole(role);
+		;
+		id.setBibliotheque(bibliotheque);
+		AvoirRole avoirRole = new AvoirRole();
+		avoirRole.setId(id);
+		return avoirRoleRepository.saveAndFlush(avoirRole);
+	}
+
+	@Override
+	public void retirerRoleUtilisateur(Utilisateur utilisateur, Role role, Bibliotheque bibliotheque) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Collection<Role> getRolesUtilisateur(Utilisateur utilisateur, Bibliotheque bibliotheque) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
