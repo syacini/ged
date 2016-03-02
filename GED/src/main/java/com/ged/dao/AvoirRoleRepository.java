b5fbf65f0c94b8b52c28debdce789fdc46418a80
Samir <Samir@MacBook>
2016-03-02 02:39:42 +0100

    Correction de la requête nommée

--- GED/src/main/java/com/ged/dao/AvoirRoleRepository.java
+++ GED/src/main/java/com/ged/dao/AvoirRoleRepository.java
@@ -15,9 +15,9 @@ import com.ged.domain.Utilisateur;
 
 @Repository
 public interface AvoirRoleRepository extends JpaRepository<AvoirRole, AvoirRolePK> {
-        @Query("SELECT a.role FROM AvoirRole a WHERE a.utilisateur = :u AND a.bibliotheque = :b")
+        @Query("SELECT a.id.role FROM AvoirRole a WHERE a.id.utilisateur = :u AND a.id.bibliotheque = :b")
         public Collection<Role> getRolesUtilisateur(@Param("u") Utilisateur utilisateur, @Param("b") Bibliotheque bibliotheque);
         
-        @Query("DELETE AvoirRole a WHERE a.utilisateur = :u AND a.role = :r AND a.bibliotheque = :b")
+        @Query("DELETE AvoirRole a WHERE a.id.utilisateur = :u AND a.id.role = :r AND a.id.bibliotheque = :b")
         public void retirerRoleUtilisateur(@Param("u") Utilisateur utilisateur, @Param("r") Role role, @Param("b") Bibliotheque bibliotheque);
 }
